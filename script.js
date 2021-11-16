2c5dea5f6f64acdd6c8c7907ded077bee50905ef
weijian000 <weijian1311@gmail.com>
2021-11-16 23:02:39 +0800

    basic calculator edit

--- script.js
+++ script.js
@@ -1,6 +1,25 @@
 function compute()
 {
-    p = document.getElementById("principal").value;
+    let amountval=document.getElementById("amount").value;
+    let rateval = document.getElementById("rate").value;
+    let yearsval = document.getElementById("years").value;
+    let date = new Date();
+    let dateval= parseInt(date.getFullYear())+parseInt(yearsval);
+   
+    let resultval = amountval*(rateval/100)*yearsval;
+    while(resultval>=0){
+    document.getElementById('result').style.display='inline';
+    document.getElementById("result1").innerHTML=amountval;
+    document.getElementById("result2").innerHTML=rateval;
+    document.getElementById("result3").innerHTML=resultval;
+    document.getElementById("result4").innerHTML= dateval;
+    }
+    
     
 }
+
+function updateTextInput(val) {
+
+    document.getElementById('textInput').innerHTML = parseFloat(val).toFixed(2)+"%"; 
+  }
         
